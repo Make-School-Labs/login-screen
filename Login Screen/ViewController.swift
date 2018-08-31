4257fcedae594e5b61370d5c50000b7485a48948
LinnierGames <esericksanc@gmail.com>
2018-08-31 08:47:38 -0700

    WIP: Segue to RegisterVC

--- Login Screen/ViewController.swift
+++ Login Screen/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    func loginUser(username: String, password: String) {
+        
+        //TODO: login user
+        
+        print("Welcome, \(username)!")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    @IBAction func pressLogin(_ sender: Any) {
+        
+        guard
+            textFieldUsername.text?.isEmpty == false,
+            let username = textFieldUsername.text,
+            textFieldPassword.text?.isEmpty == false,
+            let password = textFieldPassword.text else {
+                return print("text fields must not be empty!")
+        }
+        
+        //dismiss keyboard
+        view.endEditing(false)
+        
+        loginUser(username: username, password: password)
     }
-
-
+    
+    @IBAction func pressSignUp(_ sender: Any) {
+        
+    }
 }
 
