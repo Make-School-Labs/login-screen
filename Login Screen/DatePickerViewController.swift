f1cff66dd01e75a3d579b167931efee26c7c9c47
LinnierGames <esericksanc@gmail.com>
2018-09-14 11:30:23 -0700

    WIP: Layout Date Picker Screen

---
+++ Login Screen/DatePickerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DatePickerViewController.swift
+//  Login Screen
+//
+//  Created by Erick Sanchez on 8/31/18.
+//  Copyright © 2018 LinnierGames. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBAction func pressDone(sender: Any) {
+        
+    }
+    
+    @IBAction func pressCancel(sender: Any) {
+        
+    }
+
+}
